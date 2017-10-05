f2b9a3e05524289c014fc9f6850983c145a57855
Elise Bassett <elisebassett@Elises-MacBook-Air.local>
2017-10-05 15:18:34 -0600

    fixes #2 full assignment done

--- part_two.js
+++ part_two.js
@@ -1,1 +1,6 @@
+document.getElementById('element').addEventListener('mouseover', function (){
+	alert('Hey! I told you not to do that!!!!');
+	document.getElementById('element').innerHTML = "What did I say?";
+
+});
 
